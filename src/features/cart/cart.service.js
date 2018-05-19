c3fe57b7e3e20f9ac753e1bdf8fcb20720a73187
Jinto Jose <jintoppy@gmail.com>
2018-05-19 11:20:04 +0530

    adding event aggregator

--- src/features/cart/cart.service.js
+++ src/features/cart/cart.service.js
@@ -1,14 +1,17 @@
 import {HttpClient} from 'aurelia-fetch-client';
 import  { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
-@inject(HttpClient)
+@inject(HttpClient, EventAggregator)
 export class CartService{
     cart = [];
-    constructor(http){
+    constructor(http, ea){
         this.http = http;
+        this.ea = ea;
     }
 
     addToCart(id){
         this.cart.push(id);
+        this.ea.publish('addedToCart', this.cart.length);
     }
 }
--- src/features/cart/mini-cart/mini-cart.js
+++ src/features/cart/mini-cart/mini-cart.js
@@ -1,5 +1,13 @@
+import  { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
 export class MiniCart {     
-  constructor() {
+  count = 0;
+  constructor(ea) {
     this.message = 'Hello world';
+    ea.subscribe('addedToCart', (res) => {
+      this.count = res;
+    });
   }
 }
